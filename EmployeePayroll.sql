6b837cb8018e9685f7969a3812a356f3c7b1a88a
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-25 21:45:33 +0530

    [Shreyash]: Added query to create table employee payroll

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -1,4 +1,7 @@
 -- UC-1 : create employee_database & check created or not & use it
-create database employee_payroll;
+create database payroll_service;
 show databases;
-use employee_payroll;
+use payroll_service; 
+-- UC-2 : create table employee_payroll
+create table employee_payroll
+(id int auto_increment primary key, name varchar(50), salary int, start_date date);
