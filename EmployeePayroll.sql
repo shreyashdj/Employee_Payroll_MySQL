f499103cb38708d7a72551fc7d66cbcc928ce7f0
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-25 22:04:48 +0530

    [Shreyash]: Added query to add employee data in employee_payroll table

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -5,3 +5,10 @@ use payroll_service;
 -- UC-2 : create table employee_payroll
 create table employee_payroll
 (id int auto_increment primary key, name varchar(50), salary int, start_date date);
+-- UC-3 : insert data in table
+insert into employee_payroll (name, salary, start_date) values
+("shreyash", 34900, "2010-05-11"),
+("akash", 55700, "2005-08-11"),
+("rohit", 65200, "2000-04-07"),
+("suyog", 25500, "2020-11-08"),
+("hitesh", 46100, "2015-01-22");
