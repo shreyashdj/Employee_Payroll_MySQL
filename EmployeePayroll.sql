be550fdb104dbad9062ed437e0a3d92e9d919fa8
shreyashdj <shreyashjadhav1799@gmail.com>
2022-10-26 01:09:01 +0530

    [Shreyash]: Added query to add columns department, address, phone_number

--- EmployeePayroll.sql
+++ EmployeePayroll.sql
@@ -9,11 +9,14 @@ create table employee_payroll
 
 -- UC-3 : insert data in table
 insert into employee_payroll (name, salary, start_date) values
-("shreyash", 34900, "2010-05-11"),
-("akash", 55700, "2005-08-11"),
-("rohit", 65200, "2000-04-07"),
-("suyog", 25500, "2020-11-08"),
-("hitesh", 46100, "2015-01-22");
+("shreyash", 43600, "2006-08-14"),
+("sayali", 34900, "2014-03-24"),
+("akash", 50100, "2002-11-28"),
+("rutuja", 38300, "2010-03-07"),
+("rohit", 29500, "2018-06-17"),
+("ankita", 56200, "1998-10-11"),
+("hitesh", 22800, "2022-01-02"),
+("sneha", 34700, "2010-12-21");
 
 -- UC-4 : retrieve all data from table
 select * from employee_payroll;
@@ -24,8 +27,8 @@ select id,name,salary from employee_payroll where start_date between cast("2014-
 
 -- UC-6 : add new column gender and set gender for all eployees
 alter table employee_payroll add gender char(1);
-update employee_payroll set gender = "M" where name = "shreyash" or name = "rohit" or name = "hitesh";
-update employee_payroll set gender = "F" where name = "sayali" or name = "rutuja" or name = "ankita";
+update employee_payroll set gender = "M" where name = "shreyash" or name = "rohit" or name = "hitesh" or name = "akash";
+update employee_payroll set gender = "F" where name = "sayali" or name = "rutuja" or name = "ankita" or name = "sneha";
 
 -- UC-7 : find salary sum, average, min, max, count by gender
 select gender, sum(salary) from Employee_Payroll Where gender = 'M' group by gender;
@@ -38,3 +41,10 @@ select gender, max(salary) from Employee_Payroll Where gender = 'M' group by gen
 select gender, max(salary) from Employee_Payroll Where gender = 'F' group by gender;
 select gender, count(name) from Employee_Payroll Where gender = 'M' group by gender;
 select gender, count(name) from Employee_Payroll Where gender = 'F' group by gender;
+
+-- UC-8 : add new columns phone, address, department
+delete from employee_payroll department;
+alter table employee_payroll add department varchar(50) not null after name;
+alter table employee_payroll add address varchar(50) after department;
+alter table employee_payroll add phone_number varchar(50) after address;
+alter table employee_payroll alter address set default "default";
